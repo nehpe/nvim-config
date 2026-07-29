84a4c4026563ea5c9767bd488cb5a71afcf59f4f
nehpe <n@nehpe.com>
2026-07-28 20:47:27 -0600

    chore: initialize Neovim configuration

---
+++ README.md
@@ -0,0 +1,9 @@
+# nvim-config
+
+A small Neovim configuration for Windows and macOS.
+
+## Requirements
+
+- Neovim 0.11 or newer
+- Git
+
---
+++ lua/config/options.lua
@@ -0,0 +1,23 @@
+local opt = vim.opt
+
+opt.number = true
+opt.mouse = "a"
+opt.clipboard = "unnamedplus"
+opt.breakindent = true
+opt.undofile = true
+opt.ignorecase = true
+opt.smartcase = true
+opt.signcolumn = "yes"
+opt.updatetime = 250
+opt.timeoutlen = 300
+opt.splitright = true
+opt.splitbelow = true
+opt.confirm = true
+opt.termguicolors = true
+
+opt.tabstop = 4
+opt.softtabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+opt.smartindent = true
+

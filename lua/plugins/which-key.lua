4b8232e7adf88daee57cdbb9bbb875e07f08d534
nehpe <n@nehpe.com>
2026-07-28 20:51:40 -0600

    feat: add keymap discovery

---
+++ lua/plugins/which-key.lua
@@ -0,0 +1,17 @@
+return {
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  opts = {
+    delay = 300,
+    icons = {
+      mappings = false,
+      rules = false,
+    },
+    spec = {
+      { "<leader>c", group = "Code" },
+      { "<leader>f", group = "Find" },
+      { "<leader>g", group = "Git" },
+      { "<leader>t", group = "Toggle" },
+    },
+  },
+}

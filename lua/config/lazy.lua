6303e8d63d6248b874e52daffcd04d2bc78ae3c0
nehpe <n@nehpe.com>
2026-07-28 20:48:31 -0600

    feat: add plugin management

---
+++ lua/config/lazy.lua
@@ -0,0 +1,24 @@
+local lazypath = vim.fs.joinpath(vim.fn.stdpath("data"), "lazy", "lazy.nvim")
+
+if not vim.uv.fs_stat(lazypath) then
+  local output = vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "--branch=stable",
+    "https://github.com/folke/lazy.nvim.git",
+    lazypath,
+  })
+
+  if vim.v.shell_error ~= 0 then
+    error("Failed to install lazy.nvim:\n" .. output)
+  end
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  spec = {},
+  change_detection = { notify = false },
+  checker = { enabled = false },
+})
